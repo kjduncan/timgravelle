b1c447c96975589220e8ddd7dfc0458cc15b1746
Kate <duncan.katelynj@gmail.com>
2018-01-02 14:05:38 -0700

    footer added, some updates on home page

---
+++ the-real-montana/footer.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?>
+	</div><!-- #main .wrapper -->
+	<footer id="colophon" role="contentinfo">
+		<div class="site-info">
+			<div class="row">
+				<div class="columns small-12 large-6">
+					<p><strong>TIM GRAVELLE - GLACIER REAL ESTATE</strong></p>
+					<p>Brokers License RRE-BRO-LIC-19032</p>
+					<p>Glacier Real Estate 428 East Idaho Kalispell, MT 59901</p>
+					<p>Tim.Gravelle@TheRealMontana.com</p>
+					<p>Cell: (406) 250-9803 <br>Phone: (406) 752-1555 <br>Fax: (406) 752-2661</p>
+				</div>
+				<div class="columns small-12 large-6">
+					<ul class="footer-logos">
+						<li>
+							<img src="<?php echo get_site_url(); ?>/wp-content/uploads/2017/12/equal_housing.png" alt="">
+						</li>
+						<li>
+							<img src="http://localhost:8888/wordpress/wp-content/uploads/2017/12/realtor_mls.png" alt="">
+						</li>
+						<li>
+							<img src="http://localhost:8888/wordpress/wp-content/uploads/2017/12/realtor_logo.png" alt="">
+						</li>
+						<li>
+							<img src="http://localhost:8888/wordpress/wp-content/uploads/2017/12/equator_certified.png" alt="">
+						</li>
+					</ul>
+
+				</div>
+			</div>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
